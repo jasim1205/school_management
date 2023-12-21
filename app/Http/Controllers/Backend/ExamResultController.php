5433a8f2b19cff40f31da29acc549d08f6511424
jasim1205 <jasimuddinm180@gmail.com>
2023-12-21 10:48:28 +0600

    examresult edit

--- app/Http/Controllers/Backend/ExamResultController.php
+++ app/Http/Controllers/Backend/ExamResultController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\ExamResult;
 use App\Models\Exam;
-use App\Models\STudent;
+use App\Models\Student;
 use App\Models\Classes;
 use App\Models\Section;
 use App\Models\Session;
