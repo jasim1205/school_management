58f09f59f3fde9dcf9161eb9438320ea0ea2c6e2
jasim1205 <jasimuddinm180@gmail.com>
2024-10-06 22:09:36 +0600

    auth loader off

--- resources/views/backend/layouts/appAuth.blade.php
+++ resources/views/backend/layouts/appAuth.blade.php
@@ -16,8 +16,8 @@
 	<link href="{{asset('public/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css')}}" rel="stylesheet" />
 	<link href="{{asset('public/assets/plugins/metismenu/css/metisMenu.min.css')}}" rel="stylesheet" />
 	<!-- loader-->
-	<link href="{{asset('public/assets/css/pace.min.css')}}" rel="stylesheet" />
-	<script src="{{asset('public/assets/js/pace.min.js')}}"></script>
+	{{-- <link href="{{asset('public/assets/css/pace.min.css')}}" rel="stylesheet" /> --}}
+	{{-- <script src="{{asset('public/assets/js/pace.min.js')}}"></script> --}}
 	<!-- Bootstrap CSS -->
 	<link href="{{asset('public/assets/css/bootstrap.min.css')}}" rel="stylesheet">
 	<link href="{{asset('public/assets/css/bootstrap-extended.css')}}" rel="stylesheet">
